97f8ea83e42129fdb9a901d2f226add40389991d
RebekahMars <47405076+RebekahMars@users.noreply.github.com>
2021-05-28 17:04:02 -0700

    updates, not compiling though

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import JobBoard from "./components/jobBoard"
+
+//import {useState, useEffect} from "react";
 
 function App() {
+  
+  //const[jobData, setJobData] = useState("");
+
   return (
     <div className="App">
+      <JobBoard></JobBoard>
     </div>
   );
 }
---
+++ src/components/jobBoard.js
@@ -0,0 +1,30 @@
+import jwt from "jsonwebtoken";
+import {useEffect, useState} from "react";
+
+// this token gets you access to almost all of Emsi's jpa data
+const getToken = () => jwt.sign({
+  exp: (() => Math.floor(Date.now() / 1000) + 60)(),
+  "iss": "learn-web-dev-camper-key"
+}, "web-dev-camper-secret");
+
+// Feel free to call a state setter instead of a console log
+const FetchData = () => {
+	useEffect(() => {
+		fetch("<https://emsiservices.com/emsi-open-proxy-service/postings/us/taxonomies/skills?q=metal&limit=50", {
+		  headers: {
+		    "Content-Type": "application/json",      
+		    authorization: `Bearer ${getToken()}`,
+		  },
+		}).then(res => res.json()).then(res => console.log(res))
+	}, [])
+
+	return null;
+}
+
+function JobBoard()
+{
+    return(
+    )
+}
+
+export default JobBoard;
