7d8b7726a1a92ef002cc12e8f8b50798b9310731
RebekahMars <47405076+RebekahMars@users.noreply.github.com>
2021-05-29 14:26:51 -0700

    changed data

--- src/components/jobBoard.js
+++ src/components/jobBoard.js
@@ -1,30 +1,10 @@
-import jwt from "jsonwebtoken";
-import {useEffect, useState} from "react";
-
-// this token gets you access to almost all of Emsi's jpa data
-const getToken = () => jwt.sign({
-  exp: (() => Math.floor(Date.now() / 1000) + 60)(),
-  "iss": "learn-web-dev-camper-key"
-}, "web-dev-camper-secret");
-
-// Feel free to call a state setter instead of a console log
-const FetchData = () => {
-	useEffect(() => {
-		fetch("<https://emsiservices.com/emsi-open-proxy-service/postings/us/taxonomies/skills?q=metal&limit=50", {
-		  headers: {
-		    "Content-Type": "application/json",      
-		    authorization: `Bearer ${getToken()}`,
-		  },
-		}).then(res => res.json()).then(res => console.log(res))
-	}, [])
-
-	return null;
-}
+import FetchData from "./fetchData";
 
 function JobBoard()
 {
-    return(
-        <h1>Hello</h1>
+    return (
+        <h1>{FetchData.name}</h1> //map array to <Job>, make each element--> like list data using map
+		
     )
 }
 
